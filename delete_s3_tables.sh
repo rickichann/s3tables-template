0ec0dcd4c955799f300b080ef975b409bd9bac27
devrichan <rickichidayatullah@gmail.com>
2026-03-06 15:48:52 +0700

    delete_s3_tables modify

--- delete_s3_tables.sh
+++ delete_s3_tables.sh
@@ -1,18 +1,17 @@
 # Set variables
 
-REGION="ap-xxxxxxxxx-xx"
-
-BUCKET_ARN="arn:aws:s3tables:<region>:<account-id>:bucket/<table-bucket-name>"
+REGION=""
+BUCKET_S3TABLE="arn:aws:s3tables:<region>:<account-id>:bucket/<table-bucket-name>"
 
 # Delete all tables in all namespaces
 
-for namespace in $(aws s3tables list-namespaces --region $REGION --table-bucket-arn $BUCKET_ARN --query 'namespaces[].namespace[]' --output text); do
+for namespace in $(aws s3tables list-namespaces --region $REGION --table-bucket-arn $BUCKET_S3TABLE --query 'namespaces[].namespace[]' --output text); do
 
-  for table in $(aws s3tables list-tables --region $REGION --table-bucket-arn $BUCKET_ARN --namespace $namespace --query 'tables[].name' --output text); do
+  for table in $(aws s3tables list-tables --region $REGION --table-bucket-arn $BUCKET_S3TABLE --namespace $namespace --query 'tables[].name' --output text); do
 
     echo "Deleting table: $namespace.$table"
 
-    aws s3tables delete-table --region $REGION --table-bucket-arn $BUCKET_ARN --namespace $namespace --name $table
+    aws s3tables delete-table --region $REGION --table-bucket-arn $BUCKET_S3TABLE --namespace $namespace --name $table
 
   done
 
@@ -20,14 +19,14 @@ done
 
 # Delete all namespaces
 
-for namespace in $(aws s3tables list-namespaces --region $REGION --table-bucket-arn $BUCKET_ARN --query 'namespaces[].namespace[]' --output text); do
+for namespace in $(aws s3tables list-namespaces --region $REGION --table-bucket-arn $BUCKET_S3TABLE --query 'namespaces[].namespace[]' --output text); do
 
   echo "Deleting namespace: $namespace"
 
-  aws s3tables delete-namespace --region $REGION --table-bucket-arn $BUCKET_ARN --namespace $namespace
+  aws s3tables delete-namespace --region $REGION --table-bucket-arn $BUCKET_S3TABLE --namespace $namespace
 
 done
 
 # Delete the bucket
 
-aws s3tables delete-table-bucket --region $REGION --table-bucket-arn $BUCKET_ARN
+aws s3tables delete-table-bucket --region $REGION --table-bucket-arn $BUCKET_S3TABLE
